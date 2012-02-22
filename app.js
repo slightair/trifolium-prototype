5397338d9f0f04b4b732f2c56924da765bfcec5d
Tomohiro Moro <arksutite@gmail.com>
2012-02-23 03:30:37 +0900

    port がダメとherokuで怒られるので

--- app.js
+++ app.js
@@ -29,7 +29,8 @@
 
   app.get("/", routes.index);
 
-  app.listen(3000);
+  port = process.env.PORT || 3000
+  app.listen(port);
 
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
